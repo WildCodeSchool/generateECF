8a8fed3236f59f1d65a30ac33458986f552b125b
AurelienBarbier <aurelien@wildcodeschool.fr>
2020-01-29 17:03:50 +0100

    Optimisation DB

--- src/AppBundle/Controller/DataController.php
+++ src/AppBundle/Controller/DataController.php
@@ -55,38 +55,58 @@ class DataController extends Controller
             $students[$studentFromDB->getEmail()] = $studentFromDB;
         }
 
+
+        $cities = [];
+        $citiesFromDB = $em->getRepository(City::class)->findAll();
+        /**
+         * @var $citiyFromDB City
+         */
+        foreach ($citiesFromDB as &$cityFromDB) {
+            $cities[$cityFromDB->getName()] = $cityFromDB;
+        }
+
         foreach ($crews as $crew) {
             if (new \DateTime('2018-02-20') < new \DateTime($crew->start_date) && $crew->start_date != null) {
 
                 if (key_exists($crew->name, $cohorts)) {
                     $promo = $cohorts[$crew->name];
-                }else{
+                } else {
                     $promo = new Promo();
                 }
 
-                $promo->setName($crew->name);
+                if ($promo->getName() != $crew->name) {
+                    $promo->setName($crew->name);
+                }
 
-                $promo->setStart(new \DateTime($crew->start_date));
-                $promo->setEnd(new \DateTime($crew->end_date));
+                if ($promo->getStart() != ($crewStartDate = new \DateTime($crew->start_date))) {
+                    $promo->setStart($crewStartDate);
+                }
 
-                if (count($crew->trainers) > 0) {
-                    $promo->setTrainer($crew->trainers[0]->fullname);
+                if ($promo->getEnd() != ($crewEndDate = new \DateTime($crew->end_date))) {
+                    $promo->setEnd($crewEndDate);
+                }
+
+                if ($promo->getTrainer() != ($crewEndDate = new \DateTime($crew->end_date))) {
+                    if (count($crew->trainers) > 0) {
+                        $promo->setTrainer($crew->trainers[0]->fullname);
+                    }
                 }
 
                 if (isset($crew->course)) {
-                    $promo->setLangage($crew->course->name);
-                } else {
-                    $promo->setLangage('undifined');
+                    if ($promo->getLangage() != ($crew->course->name)) {
+                        $promo->setLangage($crew->course->name);
+                    }
                 }
 
                 if (isset($crew->location->city)) {
-                    $city = $em->getRepository(City::class)->findOneBy(array('name' => $crew->location->city));
-                    if ($city == null) {
+
+                    if (!key_exists($crew->location->city, $cities)) {
                         $city = new City();
                         $city->setName($crew->location->city);
                         $em->persist($city);
+                    } else {
+                        $promo->setCity($cities[$crew->location->city]);
                     }
-                    $promo->setCity($city);
                 }
 
                 $em->persist($promo);
@@ -98,29 +118,52 @@ class DataController extends Controller
 
                     if (!key_exists($studentFromAPI->email, $students)) {
                         $student = new Student();
-                    }else{
+                    } else {
                         $student = $students[$studentFromAPI->email];
                     }
 
-                    $student->setName($studentFromAPI->lastname);
-                    $student->setFirstname($studentFromAPI->firstname);
-                    $student->setEmail($studentFromAPI->email);
-
-                    if ($studentFromAPI->gender != null) {
-                        $student->setGender($gender[$studentFromAPI->gender]);
-                    } else {
-                        $student->setGender(Student::GENDER_UNDEFINED);
+                    if ($studentFromAPI->lastname != $student->getName()) {
+                        $student->setName($studentFromAPI->lastname);
                     }
-                    $student->setPromo($promo);
-                    $student->setDateOfBirth(new \DateTime($studentFromAPI->birthdate));
+
+                    if ($studentFromAPI->firstname != $student->getFirstname()) {
+                        $student->setFirstname($studentFromAPI->firstname);
+                    }
+
+                    if ($studentFromAPI->email != $student->getEmail()) {
+                        $student->setEmail($studentFromAPI->email);
+                    }
+
+                    if ($studentFromAPI->gender != $student->getGender()) {
+                        if ($studentFromAPI->gender != null) {
+                            $student->setGender($studentFromAPI->gender);
+                        } elseif ($student->getGender() != Student::GENDER_UNDEFINED) {
+                            $student->setGender(Student::GENDER_UNDEFINED);
+                        }
+                    }
+
+                    if ($promo->getName() != $student->getPromo()->getName()) {
+                        if ($student->getPromo()->getId() < $promo->getId()) {
+                            $student->setPromo($promo);
+                        }
+                    }
+
+
+                    if (!empty($studentFromAPI->birthdate)) {
+                        if (($birthDate = new \DateTime($studentFromAPI->birthdate)) != $student->getDateOfBirth()) {
+                            $student->setDateOfBirth($birthDate);
+                        }
+                    }
+
                     $em->persist($student);
+
                 }
                 $em->flush();
             }
 
         }
         $this->addFlash('notice', 'Update BDD ok');
-
+        return $this->render('base.html.twig', []);
         return $this->redirectToRoute('homepage');
     }
 
@@ -128,7 +171,8 @@ class DataController extends Controller
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getApiCrewData()
+    private
+    function getApiCrewData()
     {
         $client = new Client();
         $res = $client->request('GET', 'https://odyssey.wildcodeschool.com/api/v2/crews', [
@@ -146,7 +190,8 @@ class DataController extends Controller
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getApiStudentData($idCrew)
+    private
+    function getApiStudentData($idCrew)
     {
         $client = new Client();
         $res = $client->request('GET', 'https://odyssey.wildcodeschool.com/api/v2/crews/' . $idCrew, [
